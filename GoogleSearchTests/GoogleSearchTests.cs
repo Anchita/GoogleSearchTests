799a62e3caf1789639b0a0c601af337d2622b619
Anchita Arora <anchita.arora@asos.com>
2021-03-30 08:53:22 +0100

    google search tests using selenium c#

---
+++ GoogleSearchTests/GoogleSearchTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace GoogleSearchTests
+{
+    public class GoogleSearchTests
+    {
+        [Test]
+        public void TestGoogleSearch()
+        {
+            IWebDriver driver = new ChromeDriver("driverpath");
+            driver.Navigate().GoToUrl("https://www.google.com");
+
+            IWebElement e = driver.FindElement(By.CssSelector("input[name='q']"));
+
+            e.SendKeys("Anchita");
+            e.Submit();
+        }
+    }
+}
